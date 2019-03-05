e3d0a3fcdf1cb6da9ac36cdf76d2200e6611503e
asl01 <alakoff@gmail.com>
2019-03-04 22:44:29 -0500

    Change for model item.js

--- models/item.js
+++ models/item.js
@@ -1,52 +1,53 @@
 module.exports = function(sequelize, DataTypes) {
-    var item = sequelize.define("item",
-      {
-        // Defining the Item model
-        itemId: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-        },
-        itemName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-        },
-        itemDesciption: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-        },
-        itemImage: {
-          type: DataTypes.BLOB
-        },
-        departmentId: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-        },
-        stockQuantity: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-        },
-        productSales: {
-          type: DataTypes.FLOAT
+  var Item = sequelize.define(
+    "item",
+    {
+      // Defining the Item model
+      itemId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        validate: {
+          len: [1]
         }
-      },{ freezeTableName: true }
-    );
-    return item;
-  };
-  
+      },
+      itemName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      itemDesciption: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      itemImage: {
+        type: DataTypes.BLOB
+      },
+      departmentId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      stockQuantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      productSales: {
+        type: DataTypes.FLOAT
+      }
+    },
+    { freezeTableName: true }
+  );
+  return Item;
+};
